f2a72efc4a7bcd3ed227fd01b6ac0d4ccc6be0c5
franovalle <francelie.ovalle@gmail.com>
2025-05-12 21:53:28 -0400

    debugging

--- routes/posts.js
+++ routes/posts.js
@@ -11,9 +11,7 @@ router.get("/:id", ensureAuth, postsController.getPost);
 
 router.post("/createPost", upload.single("file"), postsController.createPost);//this is when the order form is filed out 
 
-router.put("/likePost/:id", postsController.likePost);
-
-//router.put("/editPost/:id", postsController.editPost); 
+//router.put("/likePost/:id", postsController.likePost);//not necessary 
 
 router.delete("/deletePost/:id", postsController.deletePost);
 
