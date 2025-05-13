012e319768ce42a3e7f4968e4fb59ca0d9932344
franovalle <francelie.ovalle@gmail.com>
2025-05-12 22:53:01 -0400

    debugging

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,7 +2,7 @@
 
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
-const Comment = require("../models/Comment");
+//const Comment = require("../models/Comment");
 
 
 module.exports = {
